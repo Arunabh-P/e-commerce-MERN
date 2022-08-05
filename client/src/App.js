f7700c28bb88272ba99c95b2c60e74cb09624e2a
Arunabh-P <arunabh1995@gmail.com>
2022-08-05 22:02:25 +0530

    admin login page design

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
-
+import Routing from "./routes/Routing";
 function App() {
   return (
-    <div className="bg-black">
-      <h1 className="text-white">hello react app!</h1>
-    </div>
+    <Routing />
   );
 }
 
---
+++ client/src/screens/auth/AdminLogin.js
@@ -0,0 +1,20 @@
+const AdminLogin = () => {
+    return (
+        <div className="bg-black1 h-screen flex justify-center items-center">
+            
+            <form className="bg-black2 p-5 w-10/12 sm:w-8/12 md:w-6/12 lg:w-3/12 rounded">
+                <h3 className="mb-3 text-white capitalize font-semibold text-lg">dashboard login</h3>
+            <div className="mb-4">
+                <input type="email" name="" className="w-full bg-black1 p-4 rounded outline-none text-white" placeholder="Enter Email.." />
+            </div>
+            <div className="mb-4">
+                <input type="password" name="" className="w-full bg-black1 p-4 rounded outline-none text-white" placeholder="Enter password.." />
+            </div>
+            <div className="mb-4">
+                <input type="submit" value="sign in &rarr;" className="bg-indigo-600 w-full p-4 rounded text-white uppercase font-semibold cursor-pointer" />
+            </div>
+            </form>
+        </div>
+    )
+}
+export default AdminLogin
